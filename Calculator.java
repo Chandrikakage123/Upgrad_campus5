989125caad74fe01f95b99ca6ac469e0e71aa709
Chandrikakage123 <nikitakage5@gmail.com>
2024-01-06 16:10:01 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+
+class MyClass
+ {
+  public static void main(String args[])
+ {
+    int x, y, sum;
+    Scanner s = new Scanner(System.in); 
+    System.out.println("Type a number");
+    x = s.nextInt(); 
+
+    System.out.println("Type another number");
+    y = s.nextInt(); 
+
+    sum = x + y;  
+    System.out.println("Sum is: " + sum); 
+  }
+} 
