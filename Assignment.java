e562407e9b986d2759d2839c77d4c14a3508b241
Chandrikakage123 <nikitakage5@gmail.com>
2024-01-07 18:15:30 +0530

    first commit

---
+++ Assignment.java
@@ -0,0 +1,18 @@
+1. How to initialize a repository in Git?
+Ans: To initialize a repository to a directory in git you need to 
+     enter the "git init" command. After this command a hidden .git 
+     folder will appear.
+
+2. What is the command to add a message to a commit?
+Ans: git commit -m "".
+ 
+3. What does the git push command do?
+Ans: The git push command is used to push the content in a local
+     repository to a remote repository.
+
+4. What is the command to add git file?
+Ans: git add "file name".
+
+5. What does the "git add -A" command do?
+Ans: The git add -A command will add all modified and untracked files
+     in the entire repository.
