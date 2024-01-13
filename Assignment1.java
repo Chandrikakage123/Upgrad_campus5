8629f72b42a9cbba397a9792bdfa90ff4e992df3
Chandrikakage123 <155279866+Chandrikakage123@users.noreply.github.com>
2024-01-13 18:12:01 +0530

    first commit

---
+++ Assignment1.java
@@ -0,0 +1,104 @@
+1. How to initialize a repository in Git?
+Ans: To initialize a repository to a directory in git you need to 
+     enter the "git init" command. After this command a hidden .git 
+     folder will appear.
+
+2. What is the command to add a message to a commit?
+Ans: git commit -m "write a commit".
+ 
+3. What does the git push command do?
+Ans: The git push command is used to push the content in a local
+     repository to a remote repository.
+
+4. What is the command to add git file?
+Ans: git add "file name".
+
+5. What does the "git add -A" command do?
+Ans: The git add -A command will add all modified and untracked files
+     in the entire repository.
+
+6. write the basic java program to print "hello world"?
+Ans: public class HelloWorld
+     {
+        public static void main(String args[])
+        {
+         System.out.println("Hello world");
+         }
+     }
+
+7. What is a command for comment in java?
+Ans: two types comment
+     1.single line comment:-single line comment start with two
+       forwod slashes(//)
+     ex. //this is a comment
+          System.out.println("Hello world");
+
+      2.multi line comment:-multi line comment start with /* and
+        ends with */
+      ex. /* the code below will print the words hello world  
+          to the screen */
+
+8. Write the syntax for declaring a variable and specifying its data type?
+Ans: 1) data_type variable_name;
+       ex. int age;
+           double height;
+           char grade;
+           boolean isStudent;
+           String name;
+
+      2) data_type variable_name = initial_value;
+       ex.  int age = 60;
+           double height = 5.5;
+           char grade = 'A';
+           boolean isStudent = true;
+           String name = "john";  
+        
+
+9. Write a simple example of declaring and using variables?
+Ans: public class VariableExample
+     {
+        public static void main(String args[])
+        {
+         int myNumber = 42;
+         String myMessage;
+          
+         myMessage = "Hello java";
+         
+         System.out.println("my number is" +myNumber);
+         System.out.println("my Message is" +myMessage);
+         }
+       }
+
+10. Write a simple example of using data types?
+Ans: public class DataTypeExample
+     {
+        public static void main(String args[])
+        {
+         int age = 60;
+         double height = 5.8;
+         char grade = 'A'; 
+
+         String name = "john";
+
+       System.out.println("Age:" +age);
+       System.out.println("Height:" +height);
+       System.out.println("Grade:" +grade);  
+       System.out.println("Name:" +name);
+       }
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
